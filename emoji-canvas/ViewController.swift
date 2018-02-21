71c4a3fb9a9e230577fca92828e371347a16c0e2
EliScherrer <escherre@purdue.edu>
2018-02-21 18:31:13 -0500

    initial commit

---
+++ emoji-canvas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoji-canvas
+//
+//  Created by Eli Scherrer on 2/21/18.
+//  Copyright © 2018 Eli Scherrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
